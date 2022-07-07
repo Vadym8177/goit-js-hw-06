7f4dda420e9d8540c10e1d43cfe36b906a9548d5
Vadym8177 <vadim.8177@icloud.com>
2022-07-07 13:02:22 +0300

    Tasks 8-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onBtnChangeColor = document.querySelector('.change-color');
+const bgColorName = document.querySelector('.color');
+
+onBtnChangeColor.addEventListener('click', changeColor);
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  bgColorName.textContent = getRandomHexColor();
+}
