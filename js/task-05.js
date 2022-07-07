8e128ee92cec9039b73c4ef40f8e433e046dcc99
Vadym8177 <vadim.8177@icloud.com>
2022-07-07 21:27:33 +0300

    Task-10

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,8 @@ const output = document.querySelector('#name-output');
 
 input.addEventListener('input', onInputChange);
 
-function onInputChange (event) {
-    output.textContent = event.currentTarget.value;
+function onInputChange () {
+    output.textContent = input.value;
     if (output.textContent === '') {
         return (output.textContent = 'Anonymous');
     }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  amount = input.value;
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const newBoxes = document.createElement('div');
+    newBoxes.style.width = boxSize + 'px';
+    newBoxes.style.height = boxSize + 'px';
+    boxSize += 10;
+    newBoxes.style.backgroundColor = getRandomHexColor();
+    boxes.append(newBoxes);
+  }
+}
+
+
+function destroyBoxes(event) {
+  boxes.innerHTML = '';
+  document.querySelector('input').value = '';
+}
